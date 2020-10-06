29a74227ede4359d5cacd80975ee79597e4d4361
RHABI.Khalifa <khalifa.rhabi@ocpgroup.ma>
2020-10-06 17:04:43 +0100

    clean submit code

--- rh-management/src/Components/AddPersonForm.component.js
+++ rh-management/src/Components/AddPersonForm.component.js
@@ -2,44 +2,56 @@ import React, { useState } from "react";
 
 const AddPersonForm = props => {
   const { addNewPerson } = props;
-  const [newPerson, setNewPerson] = useState({});
+  const [newPerson, setNewPerson] = useState({
+    fullName: "", phoneNumber: "", email: "", job: ""
+  });
 
-  const handleClick = () => addNewPerson(newPerson);
-
+  const handleClick = e => {
+    e.preventDefault();
+    addNewPerson(newPerson);
+  };
   const handleInputChange = e => {
     const { name, value } = e.target;
     setNewPerson({ ...newPerson, [name]: value });
   };
 
   return (
-    <form>
+    <form onSubmit={handleClick}>
       <input
-        value={newPerson.fullName}
+        type="text" value={newPerson.fullName}
         placeholder="Full Name"
+        required
         name="fullName"
         onChange={handleInputChange}
       />
       <input
+        type="text"
         value={newPerson.phoneNumber}
         name="phoneNumber"
+        required
         placeholder="Phone Number"
         onChange={handleInputChange}
       />
       <input
+        type="text"
         value={newPerson.email}
         name="email"
+        required
         placeholder="Email"
         onChange={handleInputChange}
       />
-      <select name="job" onChange={handleInputChange}>
+      <select
+        required
+        value={newPerson.job}
+        name="job"
+        onChange={handleInputChange}
+      >
         <option value="">Job</option>
         <option value="UX Designer">UX Designer</option>
         <option value="Full-stack developer">Full-stack developer</option>
         <option value="Agile coach">Agile coach</option>
       </select>
-      <button type="button" onClick={handleClick}>
-        Add
-      </button>
+      <button type="submit">Add</button>
       <button type="reset">Reset</button>
     </form>
   );
