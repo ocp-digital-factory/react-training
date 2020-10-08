353e6445c7948f9b19d90a688a832ff7eac2ba62
RHABI.Khalifa <khalifa.rhabi@ocpgroup.ma>
2020-10-08 12:30:25 +0100

    custom theme

--- rh-management/src/App.js
+++ rh-management/src/App.js
@@ -5,22 +5,29 @@ import AppBar from "./Components/AppBar.component";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Container from "@material-ui/core/Container";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./utils/theme";
 
 function App() {
   return (
     <Router>
-      <AppBar />
-      <br/>
-      <Container>
-        <Switch>
-          <Route path="/persons">
-            <PersonsList />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <AppBar />
+        <br />
+        <Container>
+          <Switch>
+            <Route path="/profile/:email">
+              <Profile />
+            </Route>
+            <Route path="/persons">
+              <PersonsList />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </ThemeProvider>
     </Router>
   );
 }
